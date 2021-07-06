c71b3311f61702df28a27daa1cd2106ea24149af
A-AS14 <A-AS14@ad.ms.gov.pl>
2021-07-06 13:04:48 +0200

    Add main method to Calculator class.

--- src/main/java/com/kodilla/kalkulator/Calculator.java
+++ src/main/java/com/kodilla/kalkulator/Calculator.java
@@ -8,4 +8,12 @@ public class Calculator {
     public double subtract(double a, double b) {
         return a - b;
     }
+
+    public static void main(String args[]) {
+        Calculator calc = new Calculator();
+        double a = 17.2554;
+        double b = 15.44;
+        System.out.println("Result of adding " + a + " and " + b + " = " + calc.add(a, b));
+        System.out.println("Result of subtracting " + a + " and " + b + " = " + calc.subtract(a, b));
+    }
 }
